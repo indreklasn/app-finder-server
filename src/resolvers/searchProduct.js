c9d19b8cb78b4fdb07efd698f721f627b4909d1c
Indrek Lasn <lasnindrek@gmail.com>
2019-07-21 16:35:34 +0300

    search

--- src/resolvers/searchProduct.js
+++ src/resolvers/searchProduct.js
@@ -1,12 +1,12 @@
 import {Product} from '../models'
 
-export default async (parent, args) => {
-  const result = await Product.find({ "name": args.name}, (err, res) => {
+export default  (parent, args) => {
+  return Product.find({ "name": args.name}, (err, res) => {
     // console.log(res)
     console.log(err)
     console.log(res)
   })
-  return result;
+
     // .limit(25)
     // .exec((err, docs) => console.log(docs))
 }
